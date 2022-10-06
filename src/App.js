fd1f5ab8f1bcfafb2aa3083527d233bfbe2c229e
JasonXChen <jasonxchen888@gmail.com>
2022-10-05 20:45:22 -0400

    Add home and teams routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/partials/Navbar';
+import Home from './components/routes/Home';
+import Teams from './components/routes/Teams';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/teams" element={<Teams />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
